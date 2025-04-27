f6c8165def23f26593d51e9d257cf2494c8fec19
saitama-op <sanjay.jcecs@gmail.com>
2025-04-27 19:00:23 +0530

    fixed opnsense api call

--- pkg/api/client.go
+++ pkg/api/client.go
@@ -1,29 +1,29 @@
 package api
 
 import (
-    "encoding/json"
-    "net/http"
+	"encoding/json"
+	"net/http"
 )
 
-func FetchOPNSenseData(apiUrl, apiKey string) (map[string]interface{}, error) {
-    req, err := http.NewRequest("GET", apiUrl, nil)
-    if err != nil {
-        return nil, err
-    }
-    req.Header.Add("Authorization", "Bearer " + apiKey)
+func FetchOPNSenseData(apiUrl, apiKey, apiSecret string) (map[string]interface{}, error) {
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(apiKey, apiSecret)
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    var response map[string]interface{}
-    err = json.NewDecoder(resp.Body).Decode(&response)
-    if err != nil {
-        return nil, err
-    }
+	var response map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
 
-    return response, nil
+	return response, nil
 }
